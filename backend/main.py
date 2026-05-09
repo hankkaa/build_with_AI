cc0c5fcb9f495c64a5bde27ba946c5fcde8e9b4f
Hankkaa <gah0519@naver.com>
2026-05-09 12:20:57 +0900

    Refactor: Move all backend files to backend directory

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,15 @@ from models import User, TelecomPlan, CardProduct
 from routers.subscriptions import router as sub_router
 from routers.spending import router as spend_router
 from routers.swap_and_notifications import router as swap_router, router as notif_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
